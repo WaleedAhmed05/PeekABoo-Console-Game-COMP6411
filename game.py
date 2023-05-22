6bc28d6c73bcd4b83ffef3679aadc4007114d528
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-05-22 03:43:19 -0400

    Option 1 a.k.a grid printing working smoothly.

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
+import time
 
-from grid import grid
+from grid import Grid
+import os
 
-class main:
+class Main:
+    global size
+    size=4
+    global gridX
+    gridX = Grid(size)
 
-    def printScreen(self):
+    def printScreen(self,programFlag=True):
         print("---------------------\n"
               "|    PEEK-A-BOO     |\n"
               "---------------------\n")
-        grid.blankGrid(4)
-        print("Here's grid X\n") #this will print grid.
+        #Grid.randArray(4)
+        if(programFlag):
+            gridX.randArray()
+
+        gridX.blankGrid()
+        print("")
+        #print("Here's grid X\n") #this will print grid.
         print("1. Let me select two elements\n"
               "2. Uncover one element for me\n"
               "3. I give up - reveal the grid\n"
@@ -21,7 +32,39 @@ class main:
             userInput=input("Select: ")
 
             if (userInput=='1'):
-                print("1")
+                element1="x9"
+                while ((ord(element1[0]) < 97) or (ord(element1[0]) > (96 + size)) or
+                       (int(element1[1]) < 0) or (int(element1[1]) > size - 1)):  # Validating cell coordinates.
+
+                    element1 = input("Enter cell coordinates (e.g., a0): ").lower()
+                    if ((ord(element1[0]) < 97) or (ord(element1[0]) > (96 + size)) or
+                            (int(element1[1]) < 0) or (int(element1[1]) > size - 1)):
+                        print("input error: row entry is out of range for this grid. Please try again.")
+
+                element2="x9"
+                while ((ord(element2[0]) < 97) or (ord(element2[0]) > (96 + size)) or
+                       (int(element2[1]) < 0) or (int(element2[1]) > size - 1) or
+                       element1 == element2):  # Validating cell coordinates for element 2.
+
+                    element2 = input("Enter cell coordinates (e.g., b0): ").lower()
+                    if ((ord(element2[0]) < 97) or (ord(element2[0]) > (96 + size)) or
+                            (int(element2[1]) < 0) or (int(element2[1]) > size - 1) or
+                            element1 == element2):
+                        print("input error: row entry is out of range for this grid. Please try again.")
+
+                index1=((size*int(element1[1])) + (ord(element1[0])-97))
+                index2=((size*int(element2[1])) + (ord(element2[0])-97))
+
+
+                if(not gridX.checkCell(index1,index2)):
+                    gridX.updateCell(index1,index2)
+                    self.printScreen(False)
+                    time.sleep(2)
+                    gridX.switchBacktoHide(index1,index2)
+                    self.printScreen(False)
+
+
+                #gridX.updateCell(index1,index2)
                 # do this
             elif(userInput=='2'):
                 print("1")
@@ -31,11 +74,13 @@ class main:
                 # do this
             elif(userInput=='4'):
                 print("1")
+            os.system("cls")
+            self.printScreen()
                 #do this
 
 
 
-myinstance = main()
+myinstance = Main()
 myinstance.printScreen()
 myinstance.menu()
 
---
+++ grid.py
@@ -0,0 +1,87 @@
+import random
+
+
+class Grid:
+    global rand_Array,rand_flag
+    rand_Array=[] # list of random elements.
+    rand_flag=[] # to maintain visibility of grid elements
+
+
+    def __init__(self, size):
+        self.size = size
+
+
+    def updateCell(self, index1, index2):
+        rand_flag[index1]=True
+        rand_flag[index2]=True
+
+    def  checkCell(self, index1, index2):
+        if(rand_Array[index1]==rand_Array[index2]):
+            rand_flag[index1] = True
+            rand_flag[index2] = True
+            return True
+        else:
+            return False
+    def switchBacktoHide(self,index1,index2):
+        rand_flag[index1] = False
+        rand_flag[index2] = False
+        # self.blankGrid()
+        # print("test---")
+        # print(rand_flag[index1])
+
+
+
+
+
+
+    def randArray(self):
+        #size=self
+
+        number_counts = {}  # Dictionary to maintain frequency of each element.
+        while len(rand_Array) < self.size ** 2:  # Run until full grid size.
+            random_number = random.randint(1, (self.size ** 2) / 2)
+            if random_number not in number_counts:
+                number_counts[random_number] = 1
+                rand_Array.append(random_number)
+                rand_flag.append(False)
+            elif number_counts[random_number] < 2:
+                number_counts[random_number] += 1
+                rand_Array.append(random_number)
+                rand_flag.append(False)
+
+        # for i in range(size):
+        #     for j in range(size):
+        #         random_number = random.randint(1, (size ** 2) / 2)
+        #         if random_number not in number_counts:
+        #             number_counts[random_number] = 1
+        #             rand_Array[i][j]=random_number
+        #             rand_flag.append(False)
+        #         elif number_counts[random_number] < 2:
+        #             number_counts[random_number] += 1
+        #             rand_Array[i][j]=random_number
+        #             rand_flag.append(False)
+
+
+    def blankGrid(self):
+        for x in range(self.size):
+            if (x == 0):
+                print('  ', end=" ")
+            print('[' + chr(x + 65) + ']', end=" ")  # print First row
+        print(" ")
+
+        counter=0
+        for i in range(self.size):
+
+            for j in range(self.size):
+                if (j == 0):
+                    print('[' + str(i) + ']', end=" ")  # print first column
+                if(rand_flag[counter]):
+                    print(str(rand_Array[counter]), end="   ")
+                else:
+                    print("X", end="   ")
+                counter+=1
+            print(" ")
+
+
+#print(ord('A'.lower()))
+
