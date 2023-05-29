c92a6be50364bb8e3aac4e4597de8f7b5e9f65b1
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-05-28 21:27:47 -0400

    all minor bugs fixed!

--- game.py
+++ game.py
@@ -1,4 +1,11 @@
+"""
+Author: WaleedAhmed05
+Date: May 25, 2023
+Description: This file contains code for the user interface (i.e., the menu, the various user prompts,
+error messages, and the display of the final score)
+"""
 from grid import Grid
+from grid import Score
 import sys
 import time
 import os
@@ -7,6 +14,8 @@ class Main():
     size=None
     gridX=None
     score=None
+    configScreen=None
+    cheatDetector=True #check if user chose option 2 for all cells.
     unCoverCounter=0
     boardMessage=""
 
@@ -15,14 +24,20 @@ class Main():
         self.gridX = Grid(self.size)
         self.score= Score(self.size)
 
+        if os.name == 'nt':  # Windows
+            self.configScreen="cls"
+        elif os.name == 'posix':  # Linux/Unix/Mac
+            self.configScreen="clear"
+        else:
+            self.configScreen="clear" #default
+
 
     def printScreen(self,programFlag=True):
-        os.system("cls")
-        #TODO change this command to linux command.
+        os.system(self.configScreen)
         print("---------------------\n"
               "|    PEEK-A-BOO     |\n"
               "---------------------\n")
-        #Grid.randArray(4)
+
         if(programFlag):
             self.gridX.randArray()
 
@@ -36,141 +51,165 @@ class Main():
               "5. Exit\n")        #Menu.
 
     def setBoardMessage(self,message):
-        self.boardMessage=str(message)+"\n"
+        if(message==""):
+            self.boardMessage = str(message)
+        else:
+            self.boardMessage = str(message) + "\n"
+
+
 
     def menu(self):
         userInput=0
         while userInput!='5':
             userInput=input("Select: ")
-            index1=0
-            index2=0
 
-            if (userInput=='1'):
-                element1="x9"
-                while ((ord(element1[0]) < 97) or (ord(element1[0]) > (96 + self.size)) or
-                       (int(element1[1]) < 0) or (int(element1[1]) > self.size - 1)):  # Validating cell coordinates.
+
+            if (userInput=='1'): #option 1
+                element1="x9" #coordinate #1
+                firstCharflag=True
+                secondCharFlag=True
+
+                while (firstCharflag):  # Validating cell coordinates.
 
                     element1 = input("Enter cell coordinates (e.g., a0): ").lower()
-                    if ((ord(element1[0]) < 97) or (ord(element1[0]) > (96 + self.size)) or
+                    if(len(element1)!=2):
+                        print("input error: cell coordinate should be 2 characters in length! Please try again.")
+
+                    elif((not element1[0].isalpha()) or (not element1[1].isdigit())):
+                        print("input error: cell coordinate is invalid! Please try again.")
+
+                    elif((ord(element1[0]) < 97) or (ord(element1[0]) > (96 + self.size)) or
                             (int(element1[1]) < 0) or (int(element1[1]) > self.size - 1)):
                         print("input error: row entry is out of range for this grid. Please try again.")
 
-                element2="x9"
-                while ((ord(element2[0]) < 97) or (ord(element2[0]) > (96 + self.size)) or
-                       (int(element2[1]) < 0) or (int(element2[1]) > self.size - 1) or
-                       element1 == element2):  # Validating cell coordinates for element 2.
+                    else:
+                        firstCharflag=False
+
+                element2="x9" #coordinate #2
+                while (secondCharFlag):  # Validating cell coordinates for element 2.
 
                     element2 = input("Enter cell coordinates (e.g., b0): ").lower()
-                    if ((ord(element2[0]) < 97) or (ord(element2[0]) > (96 + self.size)) or
-                            (int(element2[1]) < 0) or (int(element2[1]) > self.size - 1) or
-                            element1 == element2):
+                    if (len(element2) != 2):
+                        print("input error: cell coordinate should be 2 characters in length! Please try again.")
+
+                    elif ((not element2[0].isalpha()) or (not element2[1].isdigit())):
+                        print("input error: cell coordinate is invalid! Please try again.")
+
+                    elif (element1 == element2):
+                        print("input error: duplicate entry! Please try again.")
+
+                    elif ((ord(element2[0]) < 97) or (ord(element2[0]) > (96 + self.size)) or
+                            (int(element2[1]) < 0) or (int(element2[1]) > self.size - 1)):
                         print("input error: row entry is out of range for this grid. Please try again.")
+                    else:
+                        secondCharFlag=False #correct input added / break from loop
 
 
                 index1=((self.size*int(element1[1])) + (ord(element1[0])-97))
                 index2=((self.size*int(element2[1])) + (ord(element2[0])-97))
-                self.score.countGuess(1)
+                self.score.countGuess(1) #count 1 guess
 
-            if(not self.gridX.checkCell(index1,index2)):
-                    self.gridX.updateCell(index1,index2) #TODO fix this bug.
+
+                if(not self.gridX.checkCell(index1,index2)): #if elements are not same give a glimpse for 2 seconds.
+
+                    isIndex1AlreadyVisible=self.gridX.checkVisibility(index1) #if chosen index already visible.
+                    isIndex2AlreadyVisible=self.gridX.checkVisibility(index2)
+
+                    self.gridX.updateCell(index1,index2)
                     self.printScreen(False)
                     time.sleep(2)
                     self.gridX.switchBacktoHide(index1,index2)
+                    if isIndex1AlreadyVisible: #if index was visible before, then let it remain visible.
+                            self.gridX.unCoverCell(index1)
+                    if isIndex2AlreadyVisible:
+                            self.gridX.unCoverCell(index2)
+
                     self.printScreen(False)
 
-            if (all(self.gridX.getrandFlag())):
-                self.setBoardMessage("Game Ended! Your Score! " + str(self.score.getScore()))
-                userInput = '5'
+                else: #if elements have same value, make them visible to board
+                    self.gridX.updateCell(index1, index2)
+                    self.cheatDetector=False  #player successfully selected right pairs.
+                    self.printScreen(False)
 
-                #gridX.updateCell(index1,index2)
-                # do this
+
+                if (all(self.gridX.getrandFlag())):
+                    self.setBoardMessage("Game Ended! Your Score! " + "{:.2f}".format(self.score.getScore()))
+                    self.printScreen(False)
+                    userInput = '5'
+
+
+
             elif(userInput=='2'):
-                index1 = "x9"
-                visibilityflag=True
+                option2Inputflag=True
 
-                while ((ord(index1[0]) < 97) or (ord(index1[0]) > (96 + self.size)) or
-                       (int(index1[1]) < 0) or (int(index1[1]) > self.size - 1) or
-                visibilityflag):  # Validating cell coordinates for element 2.
+                while (option2Inputflag):  # Validating cell coordinates for element 2.
+
 
                     index1 = input("Enter cell coordinates (e.g., a0): ").lower()
-                    intIndex1 = ((self.size * int(index1[1])) + (ord(index1[0]) - 97))
 
 
-                    if ((ord(index1[0]) < 97) or (ord(index1[0]) > (96 + self.size)) or
+                    if (len(index1) != 2):
+                        print("input error: cell coordinate should be 2 characters in length! Please try again.")
+
+                    elif ((not index1[0].isalpha()) or (not index1[1].isdigit())):
+                        print("input error: cell coordinate is invalid! Please try again.")
+
+
+                    elif ((ord(index1[0]) < 97) or (ord(index1[0]) > (96 + self.size)) or
                        (int(index1[1]) < 0) or (int(index1[1]) > self.size - 1)):
                         print("input error: row entry is out of range for this grid. Please try again.")
 
-                    # elif (self.gridX.checkVisibility(intIndex1)):
-                    #     print("input error: element is already visible! ")
 
                     else:
+                        intIndex1 = ((self.size * int(index1[1])) + (ord(index1[0]) - 97))
                         self.unCoverCounter+=1
-                        visibilityflag=False
+                        option2Inputflag=False
                         self.score.countGuess(2)
                         self.gridX.unCoverCell(intIndex1)
+                        self.gridX.updateUnCoverElements(intIndex1)
                         self.printScreen(False)
 
 
 
-                if (self.unCoverCounter == (self.size ** 2)):
-                    self.setBoardMessage("You cheated - Loser!. You're score is 0!")
+                if (self.unCoverCounter >= (self.size ** 2) and all(self.gridX.getunCoverElements())):
+                    self.setBoardMessage("You cheated - Loser! Your score is 0!")
                     self.printScreen()
                     userInput='5'#end game.
+
                 elif(all(self.gridX.getrandFlag())):
-                    self.setBoardMessage("Game Ended! Your Score! " + str(self.score.getScore()))
+                    self.setBoardMessage("Game Ended! Your Score! " + "{:.2f}".format(self.score.getScore()))
+                    self.printScreen()
+                    userInput='5'
 
 
 
 
-            # do this
+
             elif(userInput=='3'):
                 self.setBoardMessage("")
                 self.gridX.revealGrid()
                 self.printScreen(False)
                 userInput='5' #end game.
-                # do this
+
             elif(userInput=='4'):
-                #TODO score reset.
-                self.setBoardMessage("")
+                self.score.resetScore()
                 self.gridX.resetGame()
                 self.gridX.randArray()
+                self.setBoardMessage("")
                 self.printScreen()
-            #self.printScreen()
-
-class Score:
-
-    score=None
-    guessAttempts=0
-    minimum_possible_guesses=None
-
-    def __init__(self,sizeXx):
-        self.minimum_possible_guesses=(sizeXx**2)/2
-
-    def countGuess(self,guess):
-        self.guessAttempts+=guess
-    def getScore(self):
-        #Score = (minimum_possible_guesses / actual_guesses) * 100
-        player_score=(self.minimum_possible_guesses / self.guessAttempts)*100
-        return player_score
-    def resetScore(self):
-        self.score=0
-        self.guessAttempts=0
-
-    def updateScore(self,guess_attempt):
-        print("")
 
 
 
-sizeX=sys.argv[1]
-#Restricting the grid size to be either 2,4 or 6 to follow assignment requirement.
-if(sizeX=='2' or sizeX=='4' or sizeX=='6'):
-    myinstance = Main(int(sizeX))
-    myinstance.printScreen()
-    myinstance.menu()
-else:
-    print("Invalid Grid size!")
+if __name__ == '__main__':
+    # Code to be executed when the script is run directly
+    sizeX = sys.argv[1]
+    # Restricting the grid size to be either 2,4 or 6 to follow assignment requirement.
+    if (sizeX == '2' or sizeX == '4' or sizeX == '6'):
+        myinstance = Main(int(sizeX))
+        myinstance.printScreen()
+        myinstance.menu()
+    else:
+        print("Invalid Grid size!")
 
-#TODO - fix already visible thing and remove it
-#TODO - check input validation.
 
 
--- grid.py
+++ grid.py
@@ -2,10 +2,10 @@ import random
 
 
 class Grid:
-    #global rand_Array,rand_flag
 
     rand_Array=[] # list of random elements.
     rand_flag=[] # to maintain visibility of grid elements
+    unCoverElements=[] # list of all elements uncovered using option 2.
     size=None
 
 
@@ -25,6 +25,9 @@ class Grid:
 
     def unCoverCell(self,index1):
         self.rand_flag[index1] = True
+
+    def updateUnCoverElements(self,index1):
+        self.unCoverElements[index1] = True
 
     def revealGrid(self):
         for i in range(len(self.rand_flag)):
@@ -48,6 +51,9 @@ class Grid:
     def getrandFlag(self):
         return self.rand_flag
 
+    def getunCoverElements(self):
+        return self.unCoverElements
+
 
     def randArray(self):
         number_counts = {}  # Dictionary to maintain frequency of each element.
@@ -57,22 +63,12 @@ class Grid:
                 number_counts[random_number] = 1
                 self.rand_Array.append(random_number)
                 self.rand_flag.append(False)
+                self.unCoverElements.append(False)
             elif number_counts[random_number] < 2:
                 number_counts[random_number] += 1
                 self.rand_Array.append(random_number)
                 self.rand_flag.append(False)
-
-        # for i in range(size):
-        #     for j in range(size):
-        #         random_number = random.randint(1, (size ** 2) / 2)
-        #         if random_number not in number_counts:
-        #             number_counts[random_number] = 1
-        #             rand_Array[i][j]=random_number
-        #             rand_flag.append(False)
-        #         elif number_counts[random_number] < 2:
-        #             number_counts[random_number] += 1
-        #             rand_Array[i][j]=random_number
-        #             rand_flag.append(False)
+                self.unCoverElements.append(False)
 
 
     def blankGrid(self):
@@ -97,8 +93,27 @@ class Grid:
                     #print("X", end="     ")
                     print('{:<5}'.format("X"), end=" ")
                 counter+=1
-            #print(" \n")
 
 
-#print(ord('A'.lower()))
+class Score:
+
+    score=None
+    guessAttempts=0
+    minimum_possible_guesses=None
+
+    def __init__(self,sizeXx):
+        self.minimum_possible_guesses=(sizeXx**2)/2
+
+    def countGuess(self,guess):
+        self.guessAttempts+=guess
+    def getScore(self):
+        #Score = (minimum_possible_guesses / actual_guesses) * 100
+        player_score=(self.minimum_possible_guesses / self.guessAttempts)*100
+        return player_score
+    def resetScore(self):
+        self.score=0
+        self.guessAttempts=0
+
+    def updateScore(self,guess_attempt):
+        print("")
 
